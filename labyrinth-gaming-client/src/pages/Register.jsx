2dad43fd490b0d04dc859265f10a983c0b05207d
gwynbleiddrivia <yasinzme2@gmail.com>
2023-05-18 21:10:57 +0600

    home, blogs, all toys, login and register components created, login pages created

---
+++ labyrinth-gaming-client/src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Register = () => {
+	return (
+		<div>
+			<h2>This is Regsiter</h2>
+
+		</div>
+	);
+};
+
+export default Register;
--- labyrinth-gaming-client/src/shared/Header.jsx
+++ labyrinth-gaming-client/src/shared/Header.jsx
@@ -4,20 +4,21 @@ import {NavLink, Link} from 'react-router-dom'
 const Header = () => {
 	return (
 		<div>
-			<div className="navbar bg-emerald-950">
+			<div className="navbar bg-emerald-950 flex-col lg:flex-row justify-between pt-0 pb-0">
 			  <div className="flex">
-			    <Link to="/" className="w-1/6">
+			    <Link to="/" className="w-1/5">
 				<img src="https://i.ibb.co/PQCRSk2/editlogo.png" alt="" className=""/>
 			    </Link>
-			    <p className="text-2xl">Labyrinth Gaming | </p>
+			    <p className="text-xl">Labyrinth Gaming | </p>
 			  </div>
 			  <div className="flex-none">
-			    <ul className="flex flex-col lg:flex-row gap-1 menu menu-horizontal px-1 justify-center">
-			    	<NavLink to="/">Home</NavLink>
-			    	<NavLink to="/blogs">Blogs</NavLink>
-			    	<NavLink to="/alltoys">All Toys</NavLink>
-			    	<NavLink to="/login">Log In</NavLink>
-			    	<NavLink to="/register">Register</NavLink>
+			    <ul className="flex flex-col lg:flex-row gap-3 mr-2 menu menu-horizontal px-1 justify-center">
+			    	<NavLink to="/" className={({ isActive }) =>isActive ? "btn btn-success" : "btn btn-ghost"}>Home</NavLink>
+			    	<NavLink to="/" className={({ isActive }) =>isActive ? "btn btn-success" : "btn btn-ghost"}>Home</NavLink>
+			    	<NavLink to="/blogs" className={({ isActive }) =>isActive ? "btn btn-success" : "btn btn-ghost"}>Blogs</NavLink>
+			    	<NavLink to="/alltoys" className={({ isActive }) =>isActive ? "btn btn-success" : "btn btn-ghost"}>All Toys</NavLink>
+			    	<NavLink to="/login" className={({ isActive }) =>isActive ? "btn btn-success" : "btn btn-ghost"}>Log In</NavLink>
+			    	<NavLink to="/register" className={({ isActive }) =>isActive ? "btn btn-success" : "btn btn-ghost"}>Register</NavLink>
 			    </ul>
 			  </div>
 			</div>
