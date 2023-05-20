02fc8266734c558aea88c9f12ec2dfed05f21de8
gwynbleiddrivia <yasinzme2@gmail.com>
2023-05-20 20:01:00 +0600

    search system by toy name successfully implemented in all toys page

--- labyrinth-gaming-client/src/pages/AllToys.jsx
+++ labyrinth-gaming-client/src/pages/AllToys.jsx
@@ -21,9 +21,27 @@ const AllToys = () => {
 		setLoadedToys(t20Toys)
 		setBoolVal(!boolVal)
 	}
+	const handleSearch = event =>{
+		event.preventDefault()
+		const form = event.target
+		const query = form.query.value
+		const queriedToys = allToys.filter(toy=>toy.toyname==query)
+		setLoadedToys(queriedToys)
+	}
 	return (
 		<div className="m-auto w-fit flex flex-col gap-5">
-			 <div className="grid grid-cols-5 gap-11 align-items text-start border-t p-4 ">
+			          
+				<form className="form-control" onSubmit={handleSearch}>
+				  <div className="input-group">
+				    <input type="text" name="query" placeholder="Search by toy name" className="input input-bordered" />
+				    <button className="btn btn-square">
+				      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+				    </button>
+				  </div>
+				</form>
+
+
+				  <div className="grid grid-cols-5 gap-11 align-items text-start border-t p-4 ">
 
 							<p className="col-span-2">Toy Name</p> 	
 							<p className="col-start-3 col-span-2">Seller Name</p>	
